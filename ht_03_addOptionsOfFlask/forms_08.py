082c7d3c09ec46d2a9f9c838644beb4573a657fe
Tatiana Kalinina <tankakalinka@gmail.com>
2023-09-09 19:48:54 +0400

    hometask_03 added

--- ht_03_addOptionsOfFlask/forms_08.py
+++ ht_03_addOptionsOfFlask/forms_08.py
@@ -9,3 +9,4 @@ class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+
--- ht_03_addOptionsOfFlask/models_08.py
+++ ht_03_addOptionsOfFlask/models_08.py
@@ -19,3 +19,4 @@ class User(db.Model):
 
     def __repr__(self):
         return f'{self.firstname} {self.lastname} {self.email}'
+
